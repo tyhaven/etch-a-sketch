0fa99539a09b21b06e2161218e4ca6c6fb70f785
Tyler Haven <ty@slh1.com>
2022-05-09 17:52:45 -0600

    Permanent bgcolor change on hovered cells

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-let container = document.querySelector('.gridContainer')
-let input = document.getElementById('selection')
-let button = document.getElementById('myBtn')
-
-function testFunc () {
-    console.log('test')
-}
+let container = document.querySelector('.gridContainer');
+let input = document.getElementById('selection');
+let button = document.getElementById('myBtn');
 
 function removeChildNodes() { //Checks for and removes any old divs before adding new ones
     while (container.firstChild) {
-        container.removeChild(container.firstChild)
+        container.removeChild(container.firstChild);
     }
 }
 
 function createGrid (num) {
-    removeChildNodes()
+    removeChildNodes();
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`
-    for (let i = 0; i < num * num; i++) { //Create n divs
-        let div = document.createElement('div') 
-        div.classList.add('gridItem')
-        container.appendChild(div)
+    for (let i = 0; i < num * num; i++) { //Create n number of divs
+        let div = document.createElement('div');
+        div.classList.add('gridItem');
+        div.setAttribute('name', 'newName')
+        container.appendChild(div);
     }
 }
 
- input.addEventListener("keypress", (event) => {  //click button if enter key is pressed
+
+
+input.addEventListener("keypress", (event) => {  //click button if enter key is pressed
     if(event.key === "Enter") {
         event.preventDefault();
         button.click();
@@ -31,14 +30,21 @@ function createGrid (num) {
 })  
 
 button.addEventListener("click", () => { // get number input for grid
-    let value = Number(document.getElementById('selection').value)
+    let value = Number(document.getElementById('selection').value);
     if (value > 100) {
-        alert('Error: Number is too large. Please enter number betwee 1-100')
+        alert('Error: Number is too large. Please enter number betwee 1-100');
     } else {
-    createGrid(value)
+        createGrid(value);
+        let div = Array.from(document.getElementsByName('newName'));
+            for (let i = 0; i < div.length; i++) {
+                div[i].addEventListener('mouseover', (event) => {
+                            div[i].classList.add('colored')
+                        
+             })
+             
+        }
+
     }
 })
 
-let div = document.getElementsByClassName('gridItem')
-let newArr = []
-newArr.push(...div)
+
