6be44acb7315f529b3152a55d94a3d7f47160e1c
Tyler Haven <ty@slh1.com>
2022-05-09 11:53:03 -0600

    Create array of divs for event listener

--- script.js
+++ script.js
@@ -6,39 +6,31 @@ function testFunc () {
     console.log('test')
 }
 
-//Checks for and removes any old divs before adding new ones
-function removeChildNodes() {
+function removeChildNodes() { //Checks for and removes any old divs before adding new ones
     while (container.firstChild) {
         container.removeChild(container.firstChild)
     }
 }
 
 function createGrid (num) {
-
     removeChildNodes()
-
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`
-
-    //Create divs to change color
-    for (let i = 0; i < num * num; i++) {
+    for (let i = 0; i < num * num; i++) { //Create n divs
         let div = document.createElement('div') 
         div.classList.add('gridItem')
         container.appendChild(div)
     }
 }
 
- //click button if enter key is pressed
- input.addEventListener("keypress", (event) => {
+ input.addEventListener("keypress", (event) => {  //click button if enter key is pressed
     if(event.key === "Enter") {
         event.preventDefault();
         button.click();
     }
 })  
 
-
-
-button.addEventListener("click", () => {
+button.addEventListener("click", () => { // get number input for grid
     let value = Number(document.getElementById('selection').value)
     if (value > 100) {
         alert('Error: Number is too large. Please enter number betwee 1-100')
@@ -46,9 +38,7 @@ button.addEventListener("click", () => {
     createGrid(value)
     }
 })
-    
 
-
-
-
-
+let div = document.getElementsByClassName('gridItem')
+let newArr = []
+newArr.push(...div)
