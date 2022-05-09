89c0835f87319d53881654a79fe6be5e4f27c3d6
Tyler Haven <ty@slh1.com>
2022-05-09 09:26:34 -0600

    Add method for dynamic row/column creation

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function createGrid (num) {
     let container = document.querySelector('.gridContainer')
-    for (let i = 0; i < num; i++) {
+
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`
+
+    //Create divs to change color
+    for (let i = 0; i < num * num; i++) {
         let div = document.createElement('div') 
         div.classList.add('gridItem')
         container.appendChild(div)
-
-
-         
     }
 }
