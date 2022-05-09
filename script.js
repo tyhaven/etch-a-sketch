bcb74514052e9ae4e54c53306cc4612f3b7220c3
Tyler Haven <ty@slh1.com>
2022-05-09 10:41:22 -0600

    Add button functionality

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+let container = document.querySelector('.gridContainer')
+let input = document.querySelector('input')
+let button = document.getElementById('myBtn')
+
+//Checks for and removes any old divs before adding new ones
+function removeChildNodes() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+    //click button if enter key is pressed
+    input.addEventListener("keypress", (event) => {
+        if(event.key === "Enter") {
+            event.preventDefault();
+            button.click();
+        }
+    })  
+
+    button.addEventListener("click", () => {
+        console.log('test')
+    })
+
+function getInput (input) {
+    let numberInput = +input
+
+    
+
+}
+
+
+
 function createGrid (num) {
-    let container = document.querySelector('.gridContainer')
+
+    removeChildNodes()
 
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`
